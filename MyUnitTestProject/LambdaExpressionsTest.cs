ded09b4bb4a126d0bb9c3f495989c84390b779ad
karogabriela <87209201+karogabriela@users.noreply.github.com>
2021-09-23 20:25:37 -0600

    Lambda examples.

---
+++ MyUnitTestProject/LambdaExpressionsTest.cs
@@ -0,0 +1,42 @@
+﻿using InterviewQuestions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+
+namespace MyUnitTestProject
+{
+    [TestClass]
+    public class LambdaExpressionsTest
+    {
+        LambdaExpressions lambdaExpressions;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            lambdaExpressions = new LambdaExpressions();
+        }
+
+        [TestMethod]
+        public void GetTotalCountFromArrayTest()
+        {
+            string[] myArray = { "Uno", "Dos", "Tres", "Cuatro", "Cinco", "Seis", "Siete", "Ocho", "Nueve", "Diez" };
+            string pattern = "c";
+            int result = lambdaExpressions.GetTotalCountFromArray(myArray, pattern);
+            result.Should().Be(2);
+        }
+
+        [TestMethod]
+        public void GetTotalCountWithRegexTest()
+        {
+            string[] myArray = { "Uno", "Dos", "Tres", "Cuatro", "Cinco", "Seis", "Siete", "Ocho", "Nueve", "Diez" };
+            string pattern = "^[aeiouy]+$";
+            int result = lambdaExpressions.GetTotalCountWithRegex(myArray, pattern);
+            result.Should().Be(10);
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            lambdaExpressions = null;
+        }
+    }
+}
